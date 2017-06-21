c314f5b2cd676a4b517b6d72c22b6b33f7355d19
Tzu-ping Chung <uranusjr@gmail.com>
2017-06-22 03:32:47 +0800

    Add some edge case string tests for ascii()

--- tests/builtins/test_ascii.py
+++ tests/builtins/test_ascii.py
@@ -1,3 +1,5 @@
+import unittest
+
 from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
 
@@ -12,3 +14,19 @@ class BuiltinAsciiFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
         'test_class',
         'test_complex',
     ]
+
+    def test_latin_1(self):
+        self.assertCodeExecution("""
+            print(ascii('Piñata'))
+            """)
+
+    def test_bmp(self):
+        self.assertCodeExecution("""
+            print(ascii('リュウ・イーユー'))
+            """)
+
+    @unittest.expectedFailure
+    def test_fake_surrogates(self):
+        self.assertCodeExecution(r"""
+            print(ascii('\ud801\udc37'))
+            """)
